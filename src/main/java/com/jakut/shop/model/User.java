b29cf6e3339042bb5d73e24d401aecb44c1f768a
gintariukai <gintariukai@hotmail.com>
2023-03-24 14:08:39 +0200

    2: User Model Class, Create pages model and User

---
+++ src/main/java/com/jakut/shop/model/User.java
@@ -0,0 +1,22 @@
+package com.jakut.shop.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+}
