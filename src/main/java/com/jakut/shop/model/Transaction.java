53e94ac256671178694173faf2075363b6344199
gintariukai <gintariukai@hotmail.com>
2023-03-27 16:20:02 +0300

    2.3: User Model Class, added Transaction

---
+++ src/main/java/com/jakut/shop/model/Transaction.java
@@ -0,0 +1,28 @@
+package com.jakut.shop.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@Entity
+@Table(name = "transaction")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "purchase_date")
+    private LocalDateTime purchaseDate;
+}
