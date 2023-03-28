d08c8621b59047eff80a13c43d931dba76f2c945
gintariukai <gintariukai@hotmail.com>
2023-03-28 09:05:35 +0300

    8: User details Service Implementation and create config in service

---
+++ src/main/java/com/jakut/shop/service/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.jakut.shop.service;
+
+import com.jakut.shop.model.User;
+import com.jakut.shop.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByIdUsername(username).orElse(null);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority(user.getRole().name()));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                grantedAuthorities
+        );
+    }
+}
