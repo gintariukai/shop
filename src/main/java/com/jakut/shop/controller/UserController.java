b7bf946b4083d3c50dfe5a23d54512256d4a31c4
gintariukai <gintariukai@hotmail.com>
2023-03-28 07:25:23 +0300

    6: Create Controller package, and User Controller Implementation

---
+++ src/main/java/com/jakut/shop/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.jakut.shop.controller;
+
+import com.jakut.shop.model.Role;
+import com.jakut.shop.model.Transaction;
+import com.jakut.shop.model.User;
+import com.jakut.shop.service.ProductService;
+import com.jakut.shop.service.TransactionService;
+import com.jakut.shop.service.UserService;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.time.LocalDateTime;
+
+@Data
+@RestController
+public class UserController {
+
+    private UserService userService;
+
+    private ProductService productService;
+
+    private TransactionService transactionService;
+
+    @PostMapping("/api/user/registration")
+    public ResponseEntity<?> register(@RequestBody User user) {
+        if (userService.findByUsername(user.getUsername()) != null) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        //default role
+        user.setRole(Role.USER);
+        return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/user/login")
+    public ResponseEntity<?> getUser(Principal principal) {
+        //principal = httpServletRequest.getUserPrincipal();
+        if (principal == null || principal.getName() == null) {
+            //logout will also use here, so we should return ok http status
+            return ResponseEntity.ok(principal);
+        }
+        return new ResponseEntity<>(userService.findByUsername(principal.getName()), HttpStatus.OK);
+    }
+
+    @PostMapping("/api/user/purchase")
+    public ResponseEntity<?> purchaseProduct(@RequestBody Transaction transaction) {
+        transaction.setPurchaseDate(LocalDateTime.now());
+        return new ResponseEntity<>(transactionService.saveTransaction(transaction), HttpStatus.CREATED);
+    }
+
+    @GetMapping("api/user/products")
+    public ResponseEntity<?> getAllProducts() {
+        return new ResponseEntity<>(productService.findAllProducts(),HttpStatus.OK);
+    }
+}
