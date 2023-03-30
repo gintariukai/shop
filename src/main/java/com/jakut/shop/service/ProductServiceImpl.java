5ba98c4120c09cf61dd2d6a80a42071972614712
gintariukai <gintariukai@hotmail.com>
2023-03-30 13:12:32 +0300

    Update db files

--- src/main/java/com/jakut/shop/service/ProductServiceImpl.java
+++ src/main/java/com/jakut/shop/service/ProductServiceImpl.java
@@ -4,12 +4,14 @@ import com.jakut.shop.model.Product;
 import com.jakut.shop.repository.ProductRepository;
 import jakarta.transaction.Transactional;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Data
 @Service
+@RequiredArgsConstructor
 @Transactional
 public class ProductServiceImpl implements ProductService {
 
