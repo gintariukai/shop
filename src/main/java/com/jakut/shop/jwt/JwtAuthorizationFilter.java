f41cc40f61c8b72307cb59a7d664164a798b8e89
gintariukai <gintariukai@hotmail.com>
2023-03-30 09:42:30 +0300

    14.1: Bug fix updating Jwt files

--- src/main/java/com/jakut/shop/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/jakut/shop/jwt/JwtAuthorizationFilter.java
@@ -1,26 +1,43 @@
 package com.jakut.shop.jwt;
 
-import com.jakut.shop.service.JwtService;
+import com.jakut.shop.model.User;
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-public class JwtAuthorizationFilter implements Filter {
-    public JwtAuthorizationFilter(JwtService jwtService) {
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private static final String BEARER = "Bearer ";
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthorizationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
+
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader != null && authHeader.startsWith(BEARER)) {
+            tryAuthenticate(authHeader.replace(BEARER, ""));
+        }
+
+        filterChain.doFilter(request, response);
     }
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    private void tryAuthenticate(String jwt) {
+        User user = jwtTokenProvider.parseToken(jwt);
 
-    }
-
-    @Override
-    public void destroy() {
-        Filter.super.destroy();
+        SecurityContextHolder.getContext()
+                .setAuthentication(new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities()));
     }
 }
--- src/main/java/com/jakut/shop/jwt/JwtTokenProvider.java
+++ src/main/java/com/jakut/shop/jwt/JwtTokenProvider.java
@@ -1,8 +1,11 @@
 package com.jakut.shop.jwt;
 
+import com.jakut.shop.model.Role;
+import com.jakut.shop.model.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import jakarta.servlet.http.HttpServletRequest;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +13,8 @@ import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
+
+    public static String ROLES_CLAIM = "roles";
 
     @Value("${app.jwt.secret}")
     private String jwtSecret;
@@ -23,24 +28,35 @@ public class JwtTokenProvider {
     @Value("${app.jwt.expiration-in-ms}")
     private Long jwtExpirationInMs;
 
-    public boolean validateToken(HttpServletRequest request){
-        String token = resolveToken(request);
-        if(token == null){
-            return false;
-        }
-        Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
-        if(claims.getExpiration().before(new Date())){
-            return false;
-        }
-        return true;
+    public String createToken(User user) {
+
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setAudience("shop-client")
+                .setIssuer("shop")
+                .setSubject(user.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + jwtExpirationInMs))
+                .claim(ROLES_CLAIM, user.getRole())
+                .signWith(SignatureAlgorithm.HS512, Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                .compact();
     }
 
-    private String resolveToken(HttpServletRequest req){
-        //Bearer key...
-        String bearerToken = req.getHeader(jwtHeaderString);
-        if(bearerToken!=null && bearerToken.startsWith(jwtTokenPrefix)){
-            return bearerToken.substring(7, bearerToken.length());
-        }
-        return null;
+    public User parseToken(String jwt) {
+
+        Claims body = Jwts.parser()
+                .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                .parseClaimsJws(jwt)
+                .getBody();
+
+        String claimRole = (String) body.get(ROLES_CLAIM);
+
+        User user = new User();
+        user.setUsername(body.getSubject());
+        user.setRole(Role.valueOf(claimRole));
+
+        return user;
     }
+
 }
